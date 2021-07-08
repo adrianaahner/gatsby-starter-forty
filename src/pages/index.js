096765c4bb424d63e5b36cbf764a2813ed89007f
adrianaahner <adrianaahner@gmail.com>
2021-07-08 08:44:26 -0400

    commit to 'this is a test'

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,7 +46,7 @@ class HomeIndex extends React.Component {
                         <article style={{backgroundImage: `url(${pic03})`}}>
                             <header className="major">
                                 <h3>Magna</h3>
-                                <p>Lorem etiam nullam</p>
+                                <p>Can you see this now?/p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
