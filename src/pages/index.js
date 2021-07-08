99ba4cc6ace88f3d489f26c62d530b80cb8c1fd7
adrianaahner <adrianaahner@gmail.com>
2021-07-08 15:46:19 -0400

    updated
    original code

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,21 +32,21 @@ class HomeIndex extends React.Component {
                         <article style={{backgroundImage: `url(${pic01})`}}>
                             <header className="major">
                                 <h3>Aliquam</h3>
-                                <p>Can you see this?<p>
+                                <p>Ipsum dolor sit amet</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic02})`}}>
                             <header className="major">
                                 <h3>Tempus</h3>
-                                <p>Can you see this, too?<p>
+                                <p>feugiat amet tempus</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic03})`}}>
                             <header className="major">
                                 <h3>Magna</h3>
-                                <p>Can you see this now?</p>
+                                <p>Lorem etiam nullam</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
