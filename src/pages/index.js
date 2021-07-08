471a50e70849816b22cce0528c267b608e6b4a02
adrianaahner <adrianaahner@gmail.com>
2021-07-07 20:46:20 -0400

    new change

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ class HomeIndex extends React.Component {
                         <article style={{backgroundImage: `url(${pic02})`}}>
                             <header className="major">
                                 <h3>Tempus</h3>
-                                <p>feugiat amet tempus</p>
+                                <p>Can you see this, too?</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
